bc3af9a6e6e8a8409ecca96aed8ab60291be184f
RichardOahimire <richardoahimire@gmail.com>
2024-04-17 13:37:37 +0100

    rea

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,14 +3,15 @@ import { FiMail } from 'react-icons/fi';
 import { RiLockPasswordLine } from "react-icons/ri";
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { useAuth } from '../auth/AuthContext'; // Import the useAuth hook
+import { useAuth } from '../auth/AuthContext';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import PreventBackNavigation2 from '../components/PreventBackNavigation2';
 
 const Login = () => {
   const navigate = useNavigate();
-  const { login } = useAuth(); // Destructure the login function from useAuth
+  const { login } = useAuth();
 
-  // Function to handle form submission
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -28,30 +29,40 @@ const Login = () => {
         sessionStorage.setItem('authToken', loginResponse.token);
         sessionStorage.setItem('userId', loginResponse.id);
         sessionStorage.setItem('email', loginResponse.email);
-        login(); // Call the login function to update isLoggedIn state
+        login();
         navigate(loginResponse.role === 'Admin' ? '/admin' : '/user');
       } else {
         throw new Error('No token received');
       }
     } catch (error) {
       console.error('Login error:', error.response || error);
+      toast.error('Incorrect login details', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
   };
 
   return (
-    <div className='min-h-screen bg-[#0f1b39]'>
+    <div className='overflow-y-hidden min-h-screen bg-[#0f1b39]'>
       <PreventBackNavigation2/>
-      <div className='flex justify-center pt-36 md:pt-20'>
-        <div className='flex flex-col'>
+      <ToastContainer />
+      <div className='flex justify-center pt-32 md:pt-20'>
+        <div className='flex flex-col pt-10 md:pt-0'>
           <div className='font-bold'>
             <h1 className='text-2xl font-bold mb-10 text-white text-center'>Enter your Login Details</h1>
           </div>
-          <div className='shadow-2xl bg-white  border-4 rounded-2xl md:h-[69vh] p-16'>
+          <div className='p-8 shadow-2xl bg-white border-4 rounded-2xl md:h-[69vh] md:p-16'>
             <form onSubmit={handleSubmit} className='flex flex-col space-y-5 md:space-y-12'>
                 <div className='flex flex-col items-center'>
                     <img src="/assets/logo.svg" alt="Digital Currency" width={100} height={100} className='mx-auto' />
                     <h1 className='text-2xl italic text-blue-900 font-bold'>Payment System</h1>
-                </div> 
+                </div>
                 <div className="flex items-center border-2 border-gray-200 rounded-full p-2">
                   <FiMail className="text-black font-bold font-xl" />
                   <input type="email" name='email' placeholder="Email" required className="pl-4 border-l-2 outline-none text-sm" />
@@ -61,17 +72,17 @@ const Login = () => {
                   <input type="password" name='password' placeholder="Password" required className="pl-4 border-l-2 outline-none text-sm " />
                 </div>
                 <div>
-                  <button type='submit' className='hover:bg-[#2a3b64] text-[13px]   hover:delay-150 duration-150 bg-[#0f1b39] shadow-2xl  text-white font-bold py-4 px-8 rounded-lg w-full' > LOGIN</button>
+                  <button type='submit' className='hover:bg-[#2a3b64] text-[13px] hover:delay-150 duration-150 bg-[#0f1b39] shadow-2xl text-white font-bold py-4 px-8 rounded-lg w-full'>LOGIN</button>
                 </div>
             </form>
             <div className='pt-4 flex justify-end space-x-3'>
               <div>
-                If not Registered
-              </div> 
-              <Link to='/register' className='text-blue-900'>
-                Register now 
-              </Link> 
+                If Not Registered
               </div>
+              <Link to='/register' className='text-blue-900 font-bold'>
+                Register now
+              </Link>
+            </div>
           </div>
         </div>
       </div>
