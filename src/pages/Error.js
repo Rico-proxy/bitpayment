3cb4f43fa4d70fa6a7bb39f3930b2a44275d35a9
unknown <richardoahimire@gmail.com>
2024-04-24 16:24:37 -0700

    kll67vv

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,13 +1,19 @@
-// src/pages/ErrorPage.js
 import React from 'react';
-
-function ErrorPage() {
+import { ImEyeBlocked } from "react-icons/im";
+function Error() {
   return (
-    <div>
-      <h1>Access Denied</h1>
-      <p>You do not have permission to view this page.</p>
+    <div className="min-h-screen flex items-center justify-center" style={{backgroundImage: "url('assets/3.jpg')"}}>
+      <div className="h-[100vh] text-center font-extrabold bg-black/70
+ backdrop-brightness-75 w-full pt-[20vh]">
+        <h1 className="text-[150px] font-bold text-white">403</h1>
+        <h2 className='flex flex-row justify-center space-x-2'>
+            <ImEyeBlocked className='text-red-500 text-4xl'/>
+            <span className="text-3xl text-gray-300">Forbidden</span>
+        </h2>
+        <p className="text-xl text-gray-300">You do not have permission to view this page.</p>
+      </div>
     </div>
   );
 }
 
-export default ErrorPage;
+export default Error;
