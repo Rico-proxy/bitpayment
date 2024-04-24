6732b728eeded90d4ebe82178b06896e4052e711
unknown <richardoahimire@gmail.com>
2024-04-24 01:49:57 -0700

    dsfasffdd

--- src/components/ProtectedRouteUser.js
+++ src/components/ProtectedRouteUser.js
@@ -1,10 +1,27 @@
-// src/components/ProtectedRouteUser.js
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const ProtectedRouteUser = ({ children }) => {
     const authToken = sessionStorage.getItem('authToken');
     const userRole = sessionStorage.getItem('role');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const events = ['load', 'mousemove', 'mousedown', 'click', 'scroll', 'keypress'];
+
+        const resetTimeout = () => {
+            if (timeout) clearTimeout(timeout);
+            timeout = setTimeout(() => navigate('/login'), 300000); // 300000 ms = 5 minutes
+        };
+
+        let timeout = setTimeout(() => navigate('/login'), 300000); // Set initial timeout
+        events.forEach(event => window.addEventListener(event, resetTimeout));
+
+        return () => {
+            clearTimeout(timeout); // Clear timeout on component unmount
+            events.forEach(event => window.removeEventListener(event, resetTimeout));
+        };
+    }, [navigate]);
 
     if (!authToken || userRole !== 'User') {
         return <Navigate to="/error" replace />;
