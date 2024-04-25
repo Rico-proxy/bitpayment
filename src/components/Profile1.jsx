97e9a835ebae76393bedc3a9dcc0fa77acfcee67
unknown <richardoahimire@gmail.com>
2024-04-25 14:28:16 -0700

    kl7623133sajg

--- src/components/Profile1.jsx
+++ src/components/Profile1.jsx
@@ -49,7 +49,7 @@ const Profile1 = () => {
   }, []); // Empt
  
   return (
-    <div className="bg text-white px-2 md:px-10 md:p-6 rounded-xl shadow-lg ">
+    <div className="bg text-white px-2 md:px-6 md:p-6 rounded-xl shadow-lg ">
       <div className="flex justify-between items-center">
         <div className='flex flex-row space-x-1'>
         <span className="text-sm font-semibold">ID:</span>
@@ -89,7 +89,7 @@ const Profile1 = () => {
        <div className='flex flex-col'>
             <div className="bg-black  flex flex-row items-center mt-2 bg p-4 rounded-lg">
                 <GrLocation  className="mr-1 font-bold text-xl" />
-                <div className='flex flex-row  space-x-2 items-center text-center'>
+                <div className='flex flex-row  space-x-2 items-center text-start'>
                   <span className=''>Address</span>
                   <span className='text-sm'>{userInfo.address}</span>
                 </div>
