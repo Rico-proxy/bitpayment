c7712fac3c2988664b29e813fc2ea4f0edd32ff7
unknown <richardoahimire@gmail.com>
2024-04-25 00:01:41 -0700

    kll67vvsa2132sasasa213212221122

--- src/user/User.js
+++ src/user/User.js
@@ -176,7 +176,7 @@ const User = () => {
         </body>
         <footer className='md:pr-4  pl-20' >
                 <div className='bg md:p-3  text-white rounded-tl-xl rounded-tr-xl'>
-                  <div className='flex md:justify-center text-sm md:text-xl p-4 md:p-0'>
+                  <div className='font-bold flex md:justify-center  text-[14px] md:text-xl p-4 md:p-0'>
                   @2024 All rights reserved Bitpay Payment Systems Limited
                   </div>
                 </div>
