592f777bf6a51909fda5898964e1fe44922dda3e
unknown <richardoahimire@gmail.com>
2024-05-01 06:50:10 -0700

    lllll

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
       currentVersion={version}
       isEnabled={isProduction} //If false, the library is disabled.
       isVerboseMode={false} //If true, the library writes verbose logs to console.
-      loadingComponent={<CacheLoading />} //If not pass, nothing appears at the time of new version check.
+       //If not pass, nothing appears at the time of new version check.
       metaFileDirectory={'.'} //If public assets are hosted somewhere other than root on your server.
     >
     <StyledEngineProvider injectFirst>
